c35762556b7ef17bf201dda97f7837001bddb1ce
Jonathan Cohen <65975460+JDC1492@users.noreply.github.com>
2021-08-21 19:47:45 +0000

    Correctly Solved

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+if number % 2 == 0:
+  print('This is an even number')
+else: 
+  print('This is an odd number')
+
+# print(number % 2)
+# print(3 % 2)
 
 
 
